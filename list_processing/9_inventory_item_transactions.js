d618f189ea44f87dbfa8a9230a7d17e5b3d11829
Grace Lim <hello@gracelim.co>
2021-11-16 15:29:50 -0500

    Completes exercise 9

--- list_processing/9_inventory_item_transactions.js
+++ list_processing/9_inventory_item_transactions.js
@@ -1,25 +1,16 @@
-/*
-Problem
-=======
-
-*/
-
-
-/*
 let transactions = [ { id: 101, movement: 'in',  quantity:  5 },
-                     { id: 105, movement: 'in',  quantity: 10 },
-                     { id: 102, movement: 'out', quantity: 17 },
-                     { id: 101, movement: 'in',  quantity: 12 },
-                     { id: 103, movement: 'out', quantity: 20 },
-                     { id: 102, movement: 'out', quantity: 15 },
-                     { id: 105, movement: 'in',  quantity: 25 },
-                     { id: 101, movement: 'out', quantity: 18 },
-                     { id: 102, movement: 'in',  quantity: 22 },
-                     { id: 103, movement: 'out', quantity: 15 }, ];
+  { id: 105, movement: 'in',  quantity: 10 },
+  { id: 102, movement: 'out', quantity: 17 },
+  { id: 101, movement: 'in',  quantity: 12 },
+  { id: 103, movement: 'out', quantity: 20 },
+  { id: 102, movement: 'out', quantity: 15 },
+  { id: 105, movement: 'in',  quantity: 25 },
+  { id: 101, movement: 'out', quantity: 18 },
+  { id: 102, movement: 'in',  quantity: 22 },
+  { id: 103, movement: 'out', quantity: 15 }, ];
 
-transactionsFor(101, transactions);
-// returns
-// [ { id: 101, movement: "in",  quantity:  5 },
-//   { id: 101, movement: "in",  quantity: 12 },
-//   { id: 101, movement: "out", quantity: 18 }, ]
-*/
+function transactionsFor(val, arr) {
+  return arr.filter(obj => obj['id'] === val);
+}
+
+console.log(transactionsFor(101, transactions));
